84be00d06aba24f9412ce0b9b4533c17c891d181
Raihat Zaman Neloy <raihatneloy1992@gmail.com>
2018-08-18 14:48:19 +0600

    Add dummy data in App component for ToDoList

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 
 import Header from './../header/Header';
-import logo from './../../logo.svg';
+import ToDoList from './../todolist/ToDoList';
 import './App.css';
 
 class App extends Component {
   render() {
+    var toDoList = [
+        {
+            'completed': true,
+            'text': 'Element 1',
+        },
+        {
+            'completed': false,
+            'text': 'Element 2'
+        }
+    ];
     return (
       <div className="App">
         <Header />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ToDoList todolist={toDoList}/>
       </div>
-    );
+  );
   }
 }
 
